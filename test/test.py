9150528b0d44d0faf5d95cf27d74a4b065d54206
Anna Baydina <anna.zimina@phystech.edu>
2020-05-01 15:52:12 +0300

    add video2video transform

--- test/test.py
+++ test/test.py
@@ -1,5 +1,60 @@
 from yolo_model.yolov3 import YoloV3
 
-m = YoloV3()
-# m.evaluate(r'f:\Pictures\ItalyNov2019\Rome2019\_DSC9636.jpg')
-m.evaluate(r'/Users/annabaydina/Downloads/Telegram Desktop/photo_2020-05-01_14-18-08.jpg')
+
+from timeit import default_timer as timer
+import numpy as np
+from PIL import Image
+import cv2
+
+def detect_video(model: YoloV3, video_path, output_path=""):
+
+    vid = cv2.VideoCapture(video_path)
+    if not vid.isOpened():
+        raise IOError("Couldn't open webcam or video")
+    video_FourCC    = int(vid.get(cv2.CAP_PROP_FOURCC))
+    video_fps       = vid.get(cv2.CAP_PROP_FPS)
+    video_size      = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                        int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    isOutput = True if output_path != "" else False
+    if isOutput:
+        print("!!! TYPE:", type(output_path), type(video_FourCC), type(video_fps), type(video_size))
+        out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
+    accum_time = 0
+    curr_fps = 0
+    fps = "FPS: ??"
+    prev_time = timer()
+    frame_idx=0
+    while True:
+        frame_idx += 1
+        if frame_idx %100 != 0:
+            continue
+        return_value, frame = vid.read()
+        image = Image.fromarray(frame)
+
+        image = model.evaluate_pil(image)
+
+        result = np.asarray(image)
+        curr_time = timer()
+        exec_time = curr_time - prev_time
+        prev_time = curr_time
+        accum_time = accum_time + exec_time
+        curr_fps = curr_fps + 1
+        if accum_time > 1:
+            accum_time = accum_time - 1
+            fps = "FPS: " + str(curr_fps)
+            curr_fps = 0
+        cv2.putText(result, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.50, color=(255, 0, 0), thickness=2)
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+        cv2.imshow("result", result)
+        if isOutput:
+            out.write(result)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__=='__main__':
+    m = YoloV3()
+    detect_video(m, r'/Users/annabaydina/Downloads/Telegram Desktop/IMG_0181.MOV', '/Users/annabaydina/Downloads/Telegram Desktop/output.mp4')
+    # m.evaluate(r'f:\Pictures\ItalyNov2019\Rome2019\_DSC9636.jpg')
+    # m.evaluate(r'/Users/annabaydina/Downloads/Telegram Desktop/photo_2020-05-01_14-18-08.jpg')
--- yolo_model/yolov3.py
+++ yolo_model/yolov3.py
@@ -1,15 +1,19 @@
+import colorsys
+
+import numpy as np
 import torch
 import torch.utils
 from torch.autograd import Variable
 import torchvision.transforms as transforms
 from zoo.pytorch_yolo_v3.models import Darknet, parse_data_config
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 from zoo.pytorch_yolo_v3.utils.datasets import pad_to_square, resize
 from zoo.pytorch_yolo_v3.utils.utils import non_max_suppression, load_classes
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+
 
 
 class YoloV3(object):
@@ -26,12 +30,30 @@ class YoloV3(object):
         self.model.load_darknet_weights(weights_path)
         self.img_size = img_size
         self.class_names = load_classes(coco_names)
+
+        hsv_tuples = [(x / len(self.class_names), 1., 1.)
+                      for x in range(len(self.class_names))]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+        self.colors = list(
+            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+                self.colors))
+        np.random.seed(10101)  # Fixed seed for consistent colors across runs.
+        np.random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
+        np.random.seed(None)
+
         self.pad = (0, 0, 0, 0)
         self.zoom = 0
 
-    def tranform_image(self, image_file):
+    def tranform_image(self, image):
         Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-        img = transforms.ToTensor()(Image.open(image_file).convert('RGB'))
+
+        if isinstance(image, str):
+            img = transforms.ToTensor()(Image.open(image).convert('RGB'))
+        elif isinstance(image, Image.Image):
+            img = transforms.ToTensor()(image.convert('RGB'))
+        else:
+            raise ValueError('hui')
+
         img, self.pad = pad_to_square(img, 0)
         self.zoom = img.shape[1] / self.img_size
         img = resize(img, self.img_size)
@@ -86,3 +108,68 @@ class YoloV3(object):
         plt.show()
 
         return
+
+    def evaluate_pil(self, image) -> Image:
+        self.model.eval()
+
+        img = self.tranform_image(image)
+
+        with torch.no_grad():
+            outputs = self.model(img)
+            print(outputs.shape)
+            outputs = non_max_suppression(outputs, conf_thres=0.001, nms_thres=0.5)
+            print(len(outputs), outputs[0].shape)
+
+        for sample_i in range(len(outputs)):
+            if outputs[sample_i] is None:
+                continue
+
+            output = np.array(outputs[sample_i])
+            pred_boxes = output[:, :4]
+            pred_scores = output[:, 4]
+            pred_labels = output[:, -1]
+
+            font = ImageFont.truetype(font='SFNSDisplayCondensed-Heavy.otf', size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+            thickness = (image.size[0] + image.size[1]) // 300
+
+            for i in range(len(pred_scores)):
+                if pred_scores[i] < 0.8:
+                    break
+                x1, y1, x2, y2 = pred_boxes[i] * self.zoom
+                x1 -= self.pad[0]
+                x2 -= self.pad[0]
+                y1 -= self.pad[2]
+                y2 -= self.pad[2]
+
+                # width, height = x2 - x1, y2 - y1
+
+                class_index = int(pred_labels[i])
+                label_name = self.class_names[class_index]
+                label = "%s (%.3f)" % (label_name, float(pred_scores[i]))
+                draw = ImageDraw.Draw(image)
+                label_size = draw.textsize(label, font)
+
+                top, left, bottom, right = y1, x1, y2, x2
+                top = max(0, np.floor(top + 0.5).astype('int32'))
+                left = max(0, np.floor(left + 0.5).astype('int32'))
+                bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+                right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+                print(label, (left, top), (right, bottom))
+
+                if top - label_size[1] >= 0:
+                    text_origin = np.array([left, top - label_size[1]])
+                else:
+                    text_origin = np.array([left, top + 1])
+
+                # My kingdom for a good redistributable image drawing library.
+                for j in range(thickness):
+                    draw.rectangle(
+                        [left + j, top + j, right - j, bottom - j],
+                        outline=self.colors[class_index])
+                draw.rectangle(
+                    [tuple(text_origin), tuple(text_origin + label_size)],
+                    fill=self.colors[class_index])
+                draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+                del draw
+
+        return image
